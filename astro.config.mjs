931a107385a39ce7f6fd4e5393f057cbefeb1826
Hugo Boyano <hnboyano@gmail.com>
2024-12-09 03:06:54 +0100

    initial commit

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,13 @@
 // @ts-check
-import { defineConfig } from 'astro/config';
-import Unocss from 'unocss/astro';
+import { defineConfig } from "astro/config";
+import Unocss from "unocss/astro";
 
-import icon from 'astro-icon';
+import icon from "astro-icon";
 
 // https://astro.build/config
 export default defineConfig({
-    integrations: [Unocss({ injectReset: true, injectEntry: true }), icon()]
+  integrations: [Unocss({ injectReset: true, injectEntry: true }), icon()],
+  devToolbar: {
+    enabled: false
+  }
 });
--- src/consts.ts
+++ src/consts.ts
@@ -1,10 +1,1 @@
-/**
- * Any sort of sitewide constants should be kept here following the same
- * naming convention of all caps snakecase
- * 
- * e.g.
- * 
- * export const YEAR_ESTABLISHED = 2020
- */
-
-export const SITE_NAME = 'Company Name';
+export const SITE_NAME = 'MercApp';
